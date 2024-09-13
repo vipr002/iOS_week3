bc175fa5219c3861a0c911e5c700c196a7b5d0ec
Victoria Prigel <129780601+vipr002@users.noreply.github.com>
2024-09-13 14:18:08 +0200

    Initial Commit

---
+++ 456/_56App.swift
@@ -0,0 +1,17 @@
+//
+//  _56App.swift
+//  456
+//
+//  Created by Victoria Prigel on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct _56App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
